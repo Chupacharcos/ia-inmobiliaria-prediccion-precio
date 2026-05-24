d253d0718c7fd47a011ee1e878370e9311718b73
Chupacharcos <48329953+Chupacharcos@users.noreply.github.com>
2026-05-24 06:56:48 +0000

    feat: Mejora de la Predicción de Precio Inmobiliario [NeuralOps ProjectImprover v4.0]

---
+++ predictor.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+class Predictor:
+    def __init__(self):
+        self.model = RandomForestRegressor()
+    def train(self, data):
+        X = data.drop('price', axis=1)
+        y = data['price']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+    def predict(self, data):
+        return self.model.predict(data)
